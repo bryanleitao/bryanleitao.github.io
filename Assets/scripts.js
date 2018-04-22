75da36ef8aa36c34f6491850a199df59a885c7dc
bryanleitao <bryanleitao1991@gmail.com>
2018-04-21 22:18:43 -0300

    media querys y ajuste de ancho

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -2,10 +2,12 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
+    if (screen.width > 360){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
     }
 }
 
