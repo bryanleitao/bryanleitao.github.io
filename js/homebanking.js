c61b2155a79c5c5b50e168891040e3605415b653
bryanleitao <bryanleitao1991@gmail.com>
2018-04-22 23:46:25 -0300

    home banking modificacion de funciones de extraccion con validadores

--- js/homebanking.js
+++ js/homebanking.js
@@ -1,7 +1,8 @@
 //Declaración de variables
 var nombreUsuario =  "Bryan A. Leitao";
-var saldoCuenta = 1000;
+var saldoCuenta = 400;
 var limiteExtraccion = 200;
+var billetesDisponibles = 100;
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML
 cargarNombreEnPantalla();
@@ -9,16 +10,71 @@ actualizarSaldoEnPantalla();
 actualizarLimiteEnPantalla();
 
 //Funciones que tenes que completar
+
+function validacionExtraccion(dinero) {
+    var error = {"codigo" : 1, 
+                "mensaje" : "Ok"
+                };
+
+    console.log(error.codigo);
+    if(dinero > limiteExtraccion){
+        if(saldoCuenta <= 0){
+            if((dinero % billetesDisponibles) != 0 ){
+                error.codigo = 2;
+                error.mensaje = "Puede hacer extracciones multiplos de $" + billetesDisponibles;
+                console.log(error.codigo);
+            }
+            error.codigo = 3;
+            error.mensaje = "Su saldo es de $" + saldoCuenta;
+            console.log(error.codigo);
+        }
+        error.codigo = 3;
+        error.mensaje = "Solo puede extraer hasta $" + limiteExtraccion;
+        console.log(error.codigo);
+    }
+
+    return error;
+}
+
 function cambiarLimiteDeExtraccion() {
 
 }
 
 function extraerDinero() {
 
+    var dinero = parseInt(prompt("Cuanto desea extraer? limite: $" + limiteExtraccion)); 
+    var saldoAnterior = saldoCuenta;
+    
+    
+    var validacion = validacionExtraccion(dinero);
+    console.log(validacion[0]);
+
+    if(validacion.codigo == 1){
+        saldoCuenta -= dinero;
+        
+        alert("Has extraido: $" + dinero + 
+        "\nSaldo anterior: $" + saldoAnterior +
+        "\nSaldo actual: $" + saldoCuenta);
+
+        actualizarSaldoEnPantalla();
+    }else{
+        alert(validacion.mensaje);
+    }
+
 }
 
 function depositarDinero() {
 
+        var dinero = parseInt(prompt("Cuanto desea depositar?")); 
+
+        var saldoAnterior = saldoCuenta;
+
+        saldoCuenta += dinero;
+
+        alert("Has depositado: $" + dinero + 
+        "\nSaldo anterior: $" + saldoAnterior +
+        "\nSaldo actual: $" + saldoCuenta);
+        actualizarSaldoEnPantalla();
 }
 
 function pagarServicio() {
