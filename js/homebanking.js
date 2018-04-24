658f67095f694672310583e64aaf8525efcec5c7
bryanleitao <bryanleitao1991@gmail.com>
2018-04-24 11:20:46 -0300

    se arreglaron validaciones, se modularizo

--- js/homebanking.js
+++ js/homebanking.js
@@ -3,6 +3,7 @@ var nombreUsuario =  "Bryan A. Leitao";
 var saldoCuenta = 400;
 var limiteExtraccion = 200;
 var billetesDisponibles = 100;
+var error = actualizarError();
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML
 cargarNombreEnPantalla();
@@ -11,43 +12,25 @@ actualizarLimiteEnPantalla();
 
 //Funciones que tenes que completar
 
-function validacionExtraccion(dinero) {
-    var error = {"codigo" : 1, 
-                "mensaje" : "Ok"
-                };
-
-    console.log(error.codigo);
-    if(dinero > limiteExtraccion){
-        if(saldoCuenta <= 0){
-            if((dinero % billetesDisponibles) != 0 ){
-                error.codigo = 2;
-                error.mensaje = "Puede hacer extracciones multiplos de $" + billetesDisponibles;
-                console.log(error.codigo);
-            }
-            error.codigo = 3;
-            error.mensaje = "Su saldo es de $" + saldoCuenta;
-            console.log(error.codigo);
-        }
-        error.codigo = 3;
-        error.mensaje = "Solo puede extraer hasta $" + limiteExtraccion;
-        console.log(error.codigo);
-    }
-
-    return error;
-}
 
 function cambiarLimiteDeExtraccion() {
-
+    var dinero = parseInt(prompt("De cuanto desea el limite de extraccion? minimo $100"));
+    
+    if(dinero >= 100){
+        limiteExtraccion = dinero; 
+        actualizarLimiteEnPantalla();
+    }else alert("Ingrese una denominacion mayor o igual a $100");
 }
 
 function extraerDinero() {
 
     var dinero = parseInt(prompt("Cuanto desea extraer? limite: $" + limiteExtraccion)); 
     var saldoAnterior = saldoCuenta;
+    var validacion;
     
-    
-    var validacion = validacionExtraccion(dinero);
-    console.log(validacion[0]);
+    validacion = validacionExtraccion(dinero);
+    validacion = validacionBilletes(dinero);
+    validacion = validacionSaldo(dinero);
 
     if(validacion.codigo == 1){
         saldoCuenta -= dinero;
@@ -61,12 +44,12 @@ function extraerDinero() {
         alert(validacion.mensaje);
     }
 
+    error = actualizarError();
 }
 
 function depositarDinero() {
 
         var dinero = parseInt(prompt("Cuanto desea depositar?")); 
-
         var saldoAnterior = saldoCuenta;
 
         saldoCuenta += dinero;
@@ -78,6 +61,16 @@ function depositarDinero() {
 }
 
 function pagarServicio() {
+    // var opcion = prompt("Que servicio desea abonar?" +
+    //                     "\n1- Agua = $100"+
+    //                     "\n2- Looz = $1500"+
+    //                     "\n3- Internet = $500"+
+    //                     "\n4- Gas = $300"
+    //                     );
+    // switch(opcion){
+    //     case 1:
+    //         validacionSaldo();
+    // }
 
 }
 
@@ -102,3 +95,34 @@ function actualizarLimiteEnPantalla() {
     document.getElementById("limite-extraccion").innerHTML = "Tu límite de extracción es: $" + limiteExtraccion;
 }
 
+//Funciones propias
+
+function actualizarError(){
+    return {"codigo" : 1, 
+    "mensaje" : "Ok"
+    };
+}
+
+function validacionExtraccion(dinero) {
+    if(dinero > limiteExtraccion){
+        error.codigo = 2;
+        error.mensaje = "Solo puede extraer hasta $" + limiteExtraccion;
+    }
+    return error;
+}
+
+function validacionSaldo(dinero) {
+    if((saldoCuenta <= 0) || (saldoCuenta < dinero)){
+        error.codigo = 3;
+        error.mensaje = "Su saldo es de $" + saldoCuenta;
+    }
+    return error;
+}
+
+function validacionBilletes(dinero) {
+    if((dinero % billetesDisponibles) != 0 ){
+        error.codigo = 4;
+        error.mensaje = "Solo pueden ser dividendos multiplos de $" + billetesDisponibles;
+    }
+    return error;
+}
